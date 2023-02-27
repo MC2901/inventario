96b1b2376434ad25af983f6459129584bd9dd446
MC2901 <mcerini02@gmail.com>
2023-02-26 23:55:27 -0300

    fixed: arreglo de alta producto

--- login/ABM/altaProducto.php
+++ login/ABM/altaProducto.php
@@ -1,12 +1,10 @@
 <?php
 
-if(mysqli_connect('localhost','root','','login')){
+if (mysqli_connect('localhost', 'root', '', 'login')) {
 	//conexion a la base de datos con 4 parametros (servidor, usuarioServidor, contraseña usuario y nombre de la base de datos)
 
-	
-	
-	if(isset($_POST['codigo']) and isset($_POST['nombre']) and isset($_POST['precio']) and isset($_POST['cantidad']) and isset($_POST['desc']) and isset($_POST['detalle']) and isset($_POST['categoria'])){
-		
+	if (isset($_POST['codigo']) and isset($_POST['nombre']) and isset($_POST['precio']) and isset($_POST['cantidad']) and isset($_POST['desc']) and isset($_POST['detalle']) and isset($_POST['categoria'])) {
+
 		$codigo = $_POST['codigo'];
 		$nombre = $_POST['nombre'];
 		$precio = $_POST['precio'];
@@ -14,44 +12,28 @@ if(mysqli_connect('localhost','root','','login')){
 		$desc = $_POST['desc'];
 		$detalle = $_POST['detalle'];
 		$categoria = $_POST['categoria'];
-		
 
-		$hora=time();
-		move_uploaded_file($_FILES['foto']['tmp_name'], "../fotos/".$hora.'.jpg');
-		$foto=$hora.'.jpg';
+		$hora = time();
+		move_uploaded_file($_FILES['foto']['tmp_name'], "../fotos/" . $hora . '.jpg');
+		$foto = $hora . '.jpg';
 
-		$con = mysqli_connect('localhost','root','','login');
+		$con = mysqli_connect('localhost', 'root', '', 'login');
 		//guardo los datos de conexion
-		
+
 		$consulta = "INSERT INTO productos (codigoProducto,nombreProducto,precioProducto,cantidadProducto,fotoProducto,descripcionProducto,detallesProducto,categoriaProducto) VALUE ('$codigo','$nombre', '$precio', '$cantidad','$foto', '$desc', '$detalle', '$categoria')";
-		//guardo la consulta que quiero hacerle a la base de datos
-		
-		if($resultado = mysqli_query($con, $consulta)){
+		//guardo la consulta que quiero hacerle a la base de datos,
+
+		if ($resultado = mysqli_query($con, $consulta)) {
 			//guardo el resultado de la consulta de base de datos
-				print "<h1>El producto $nombre fue AGREGADO!!!</h1>";
-				print "<p><a href='index.php'>Inicio</a></p>";
-				
-			
+			print "<h1>El producto $nombre fue AGREGADO!!!</h1>";
+			print "<p><a href='index.php'>Inicio</a></p>";
+
 		}
-		
-		
-			
-		
-	}else{
+
+	} else {
 		print "<h1>Algo se rompio</h1>";
 	}
-		
-		
-		
-		
-		
-	
-	
-	
-	
+
 }
-
-
-
 
 ?>
--- login/ABM/productos.php
+++ login/ABM/productos.php
@@ -1,20 +1,28 @@
 <?php
 
-if(mysqli_connect('localhost','root','','login')){
-	//conexion a la base de datos con 4 parametros (servidor, usuarioServidor, contraseña usuario y nombre de la base de datos)
-	if(isset($_GET['categoria']) and isset($_GET['nombre'])){
+if (mysqli_connect('localhost', 'root', '', 'login')) {
+	//Inicializacion de variables
+	$codigo = '';
+	$nombre = '';
+	if (isset($_GET['categoria']) and isset($_GET['nombre'])) {
 		$codigo = $_GET['categoria'];
 		$nombre = $_GET['nombre'];
 	}
-	
-	$con = mysqli_connect('localhost','root','','login');
+
+	//conexion a la base de datos con 4 parametros (servidor, usuarioServidor, contraseña usuario y nombre de la base de datos)
+	if (isset($_GET['categoria']) and isset($_GET['nombre'])) {
+		$codigo = $_GET['categoria'];
+		$nombre = $_GET['nombre'];
+	}
+
+	$con = mysqli_connect('localhost', 'root', '', 'login');
 	//guardo los datos de conexion
-	
+
 	$consulta = "SELECT codigoProducto, nombreProducto FROM productos WHERE categoriaProducto='$codigo'";
-	
-	
-		//guardo la consulta que quiero hacerle a la base de datos
-	if($resultado = mysqli_query($con, $consulta)){
+
+
+	//guardo la consulta que quiero hacerle a la base de datos
+	if ($resultado = mysqli_query($con, $consulta)) {
 		//guardo el resultado de la consulta de base de datos
 		print "<table border='1'>";
 		print "<caption>$nombre</caption>";
@@ -25,28 +33,28 @@ if(mysqli_connect('localhost','root','','login')){
 					<th>Borrar</th>
 				</tr>
 		";
-		while($fila = mysqli_fetch_array($resultado) ){
+		while ($fila = mysqli_fetch_array($resultado)) {
 			//divide el resultado
 			print "<tr>";
-				print "<td> $fila[nombreProducto]</td>";
-				print "<td><a href='modProducto.php?producto=$fila[codigoProducto]'> Mod</a></td>";
-				print "<td><a href='borProducto.php?producto=$fila[codigoProducto]&&nombre=$fila[nombreProducto]'> Bor </a></td>";
-				
+			print "<td> $fila[nombreProducto]</td>";
+			print "<td><a href='modProducto.php?producto=$fila[codigoProducto]'> Mod</a></td>";
+			print "<td><a href='borProducto.php?producto=$fila[codigoProducto]&&nombre=$fila[nombreProducto]'> Bor </a></td>";
+
 			print "</tr>";
-			
-			
+
+
 		}
-		
-		
+
+
 		print "</table>";
-		
-	}else{
+
+	} else {
 		print "<h1>Algo se rompio</h1>";
 	}
-	
-	
-}else{
-	
+
+
+} else {
+
 	print "<h1>No ta funkando</h1>";
 }
 
@@ -56,51 +64,50 @@ if(mysqli_connect('localhost','root','','login')){
 ?>
 
 <form action="altaProducto.php" method="post" enctype="multipart/form-data">
-	
-	
+
+
 	<p>
 		<label for="codigo">Codigo Producto</label>
-		<input type="text" id="codigo" name="codigo"/>
+		<input type="text" id="codigo" name="codigo" />
 	</p>
-	
+
 	<p>
 		<label for="nombre">Nombre Producto</label>
-		<input type="text" id="nombre" name="nombre"/>
+		<input type="text" id="nombre" name="nombre" />
 	</p>
-	
+
 	<p>
 		<label for="precio">Precio Producto</label>
-		<input type="text" id="precio" name="precio"/>
+		<input type="text" id="precio" name="precio" />
 	</p>
-	
+
 	<p>
 		<label for="cantidad">Cantidad Producto</label>
-		<input type="text" id="cantidad" name="cantidad"/>
+		<input type="text" id="cantidad" name="cantidad" />
 	</p>
 	<p>
 		<label for="foto">Agregue una Imagen</label>
 		<input type="file" accept=".jpg" id="foto" name="foto">
 	</p>
-	
+
 	<p>
 		<textarea name="desc">Descripcion</textarea>
 	</p>
-	
+
 	<p>
 		<textarea name="detalle">Detalle</textarea>
 	</p>
-	
-	
+
+
 	<p>
 		<!-- input con la categoriaProducto -->
-		<input type="hidden" id="categoria" name="categoria" <?php print "value=$codigo";?> />
+		<input id="categoria" name="categoria" <?php print "value=$codigo"; ?> />
 	</p>
-	
-	
-	
-	
+
+
+
+
 	<input type="submit" value="Crear Producto">
 
 
 </form>
-
