dd6fa613649892abc6a221e7e532ce22c76c7e2f
MC2901 <mcerini02@gmail.com>
2023-02-28 17:22:32 -0300

    feat:  estilo portada

--- login/inventario/portada.php
+++ login/inventario/portada.php
@@ -1,8 +1,9 @@
+<link rel="stylesheet" type="text/css" href="../css/portada.css">
+</link>
 <?php
 
 //conexion a la base de datos con 4 parametros (servidor, usuarioServidor, contraseña usuario y nombre de la base de datos)
 if (mysqli_connect('localhost', 'root', '', 'login')) {
-
 	//guardo los datos de conexion
 	if (isset($_GET['categoria'])) {
 		$categoryCode = $_GET['categoria'];
@@ -11,8 +12,9 @@ if (mysqli_connect('localhost', 'root', '', 'login')) {
 		$queryDBProduct = "SELECT codigoProducto, nombreProducto, precioProducto, descripcionProducto, categoriaProducto FROM productos WHERE categoriaProducto=$categoryCode";
 		$queryDBCategory = "SELECT categoria, idCategoria FROM categorias WHERE idCategoria=$categoryCode";
 
-		
-		
+
+		print "<section class='portada'>";
+
 		//guardo la consulta que quiero hacerle a la base de datos
 		//traer el nombre de la categoria
 		if ($result = mysqli_query($connectionDB, $queryDBCategory)) {
@@ -20,30 +22,31 @@ if (mysqli_connect('localhost', 'root', '', 'login')) {
 				print "<h1>$rowDB[categoria]</h1>";
 			}
 		}
-
+		
+		print "<div class='productos'>";
 		//guardo el resultado de la consulta de base de datos
 		if ($result = mysqli_query($connectionDB, $queryDBProduct)) {
 
 			//divide el resultado
 			while ($rowDB = mysqli_fetch_array($result)) {
 
-				print "<div>";
+				print "<div class='producto'>";
 				print "<h2><a href=ficha.php?categoria=$rowDB[codigoProducto]>$rowDB[nombreProducto]</a></h2>";
 				print "<p>Precio: $rowDB[precioProducto]</p>";
 				print "<p>Descripcion: $rowDB[descripcionProducto]</p>";
 				print "</div>";
-
 			}
-
+			
+			print "</div>";
 		} else {
 			print "<h1>Algo se rompio</h1>";
 		}
 
 	}
+	print "</section>";
 
 } else {
 
 	print "<h1>Algo se rompio</h1>";
 }
-
 ?>
